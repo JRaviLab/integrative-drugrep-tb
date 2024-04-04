5b13feda2752ad8a4d174389b2fe7848a5a59f92
Landon Buskirk <lbuskirk22@gmail.com>
2024-04-03 23:02:40 -0400

    Added Stouffer z-score method to drug data grouping, but discovered it made negligible changes.

--- scripts/main.py
+++ scripts/main.py
@@ -5,9 +5,19 @@ import time
 import numpy as np
 import shelve
 import argparse
+from datetime import date
 
 
-def main(p=False, s=False, r=False, l=False):
+def main(p=False, s=False, r=False, l=False, group=None):
+
+    if not p and not s and not r and not l:
+        print("No correlation type selected. Please select at least one of the following: pearson, spearman, rbo, lasso")
+        return
+    
+    if group not in ['avg_cell_line', 'stouffer', 'none']:
+        print("Invalid group option. Please select one of the following: avg_cell_line, stouffer, none")
+        return
+    
     start = time.time()
 
     # Dataframes: [gene, sample]
@@ -19,13 +29,35 @@ def main(p=False, s=False, r=False, l=False):
     landmark_genes = drug_data.index.values
     rnaseq_dis_data, marray_dis_data = data_funcs.read_dis_data(landmark_genes)
 
-    # group drug samples by cell line -> drug_data_cell
-    cell_lines = list(set([x.split('_')[1] for x in drug_data.columns]))
-    drug_data_cell = pd.DataFrame(index=drug_data.index, columns=cell_lines)
-    for cell_line in cell_lines:
-        cell_line_columns = [x for x in drug_data.columns if cell_line in x]
-        drug_data_cell[cell_line] = drug_data[cell_line_columns].mean(axis=1)
-    drug_data = drug_data_cell
+    if group == 'avg_cell_line':
+        # group drug samples by cell line -> drug_data_cell
+        cell_lines = list(set([x.split('_')[1] for x in drug_data.columns]))
+        drug_data_cell = pd.DataFrame(index=drug_data.index, columns=cell_lines)
+        for cell_line in cell_lines:
+            cell_line_columns = [x for x in drug_data.columns if cell_line in x]
+            drug_data_cell[cell_line] = drug_data[cell_line_columns].mean(axis=1)
+        drug_data = drug_data_cell
+    elif group == 'stouffer':
+        # group drug samples by cell line -> drug_data_cell
+        cell_lines = list(set([x.split('_')[1] for x in drug_data.columns]))
+        drug_data_cell = pd.DataFrame(index=drug_data.index, columns=cell_lines)
+        for cell_line in cell_lines:
+            cell_line_columns = [x for x in drug_data.columns if cell_line in x]
+            weights = []
+            for col in cell_line_columns:
+                correlations = []
+                for col2 in cell_line_columns:
+                    if col != col2:
+                        correlations.append(np.corrcoef(drug_data[col], drug_data[col2])[0][1])
+                avg_corr = np.mean(correlations)
+                avg_corr = 0 if avg_corr<0.05 else avg_corr
+                weights.append(np.mean(correlations))
+            weights = np.array(weights)
+            weights = weights/np.sum(weights)
+            drug_data_cell[cell_line] = np.sum(drug_data[cell_line_columns].values * weights, axis=1)
+
+        drug_data = drug_data_cell
+
 
     def qnorm_dis_data(dis_data, target_vec):
         dis_data_qn = dis_data.copy()
@@ -57,7 +89,7 @@ def main(p=False, s=False, r=False, l=False):
                         for i in range(arr_3d.shape[2])]
         
         for i, mat in enumerate(corr_matrices):
-            mat.to_csv(f'{filename}{types[i][0]}.csv')
+            mat.to_csv(f'{filename}{types[i][0]}_{str(date.today())[5:]}.csv')
 
     drug_data_rna_subset = drug_data.loc[rnaseq_genes]
     drug_data_marray_subset = drug_data.loc[marray_genes]
@@ -74,7 +106,8 @@ if __name__ == "__main__":
     parser.add_argument('-s', '--spearman', action="store_true", help="Caclulate spearman correlation")
     parser.add_argument('-r', '--rbo', action="store_true", help="Caclulate rbo correlation")
     parser.add_argument('-l', '--lasso', action="store_true", help="Caclulate lasso betas")
+    parser.add_argument('-g', '--group', type=str, default=None, help="How to group drug samples. Options: avg_cell_line, stouffer, none. Default: none")
     
     args = parser.parse_args()
     
-    main(p=args.pearson, s=args.spearman, r=args.rbo, l=args.lasso)
+    main(p=args.pearson, s=args.spearman, r=args.rbo, l=args.lasso, group=args.group)
