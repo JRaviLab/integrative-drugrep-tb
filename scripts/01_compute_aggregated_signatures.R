a5da97a2188f22d256565be6647f3247bf66d7bd
Kewalin Samart <57643647+KewalinSamart@users.noreply.github.com>
2025-06-13 17:08:44 -0600

    minors, plan on sig cleaning vignette

--- scripts/01_compute_aggregated_signatures.R
+++ scripts/01_compute_aggregated_signatures.R
@@ -1,27 +1,37 @@
-# script to compute a weighted average gene vector -- an aggregated signature
-# created date: 08/31/21
-# last modified: 10/04/23
+# Script to compute a weighted average gene vector -- an aggregated signature
+# last modified: 06/13/25
 # Kewalin Samart
 
 # load needed
 library(readr)
-source("./scripts/01_signature_aggregation_functions.R")
+library(here)
+source(here("scripts/01_signature_aggregation_functions.R"))
 
 # set up arguments
-args <- commandArgs(TRUE)
-metadata_path <- args[1] # "/data/scratch/samartk/drugrep_tb/inputs/TB_microarray_args.tsv"
-data_path <- args[2] # "/data/scratch/samartk/data/uniformly_processed/microarray/signatures/"
-direction <- args[3] # direction of differential genes: "up", "dn", or "full"
-bg_source <- args[4] # name of the source for background genes to use: "LINCS", "KEGG", "GO", "input data"
-output_dir <- args[5] # path to the output directory
-extra_arg <- args[6] # extra argument; optional if "LINCS" is specified for bg_source; could be one of the options below or a combination of them as a single string with comma:
-                     # (i) "landmark" (by default) (ii) "inferred" (iii) "best inferred" (iv) "not inferred" (v) "reference" for examples: "landmark" or "landmark,inffered,best inferred"
-                     # if "input data" is specified for bg_source, this arg could be one of the followings: "up", "dn", "full", and "" (by default)
-threshold <- args[7] # aggregated gene score cutoff; 0.4 by default
-if(threshold == None){
-    threshold <- 0.4
-}
+args = commandArgs(TRUE)
 
-membership_matrix = compute_membership_matrix(metadata_path, data_path, direction, bg_source, output_dir, extra_arg)
-jaccard_matrix = compute_jaccard_matrix(metadata_path, data_path,direction, output_dir)
-aggr_signature = aggregate_signatures(membership_matrix, jaccard_matrix, output_dir, threshold=threshold)
+metadata_path = args[1] # path to metadata file for the signatures
+data_path = args[2] # path to the signatures to be aggregated
+direction = args[3] # direction of differential genes: "up", "dn", or "full" (up+dn)
+bg_source = args[4] # name of the source for background genes to use: "LINCS" (recommended for disease-drug comparison),
+                     # "KEGG", "GO", "input data"
+output_dir = args[5] # path to the output directory
+extra_arg     <- ifelse(length(args) >= 6, args[6], "") # extra argument for signature file names; "" by default
+threshold     <- ifelse(length(args) >= 7, as.numeric(args[7]), 0.4) # aggregated gene score cutoff; 0.4 by default
+
+###### Example arguments ######
+# metadata_path = here("data/TB_microarray_args.tsv")
+# data_path = here("data/microarray_TBsignatures")
+# direction = "up"
+# bg_source = "LINCS" # L1000 genes
+# extra_arg= "landmark"
+# output_dir = here("data/aggregated_signatures")
+# threshold = NULL # aggregated gene score cutoff; 0.4 by default
+
+# 1. define membership matrix
+membership_matrix = compute_membership_matrix(metadata_path, data_path, direction, bg_source, output_dir, extra_arg, save_result)
+# 2. calculate Jaccard similarity matrix
+jaccard_matrix = compute_jaccard_matrix(metadata_path, data_path,direction, output_dir, save_result)
+# 3. compute aggregated signature
+aggr_signature = aggregate_signatures(membership_matrix, jaccard_matrix, output_dir, threshold=threshold, save_result)
+
