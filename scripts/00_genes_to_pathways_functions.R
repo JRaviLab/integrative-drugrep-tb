5521e24e1421121b02ef7fb3849a58d30f5b6542
Kewalin Samart <kewalin.samart@cuanschutz.edu>
2025-07-13 15:02:11 -0600

    working code | to be cleaned up

--- scripts/00_genes_to_pathways_functions.R
+++ scripts/00_genes_to_pathways_functions.R
@@ -1,71 +1,138 @@
-# script to perform pathway analyses: GO Over-represenation analysis
+# script to perform pathway analyses: GO/KEGG/Reactome ORA
 # last modified: 07/13/25
 # Kewalin Samart
 
-library(tidyverse)
-library(here)
-library(clusterProfiler)
 library(ReactomePA)
+library(clusterProfiler)
 library(DOSE)
 library(org.Hs.eg.db)
+library(readr)
+library(here)
 
 source(here("scripts/00_background_genes_PA_functions.R"))
 
-##### PARAMETERS #####
+## set up variables
+
+technologies = c("microarray","RNAseq")
+
+metadata_path = here(paste0("data/v2/signatures/",technology,"_TB_signature_run_info.tsv"))
+signature_info = read_tsv(metadata_path)
+signature_info = signature_info[signature_info$signature == 1,]
+
+# path to DE data table/ signatures
+data_path <- here(paste0("data/v2/DE_results/", technology))
+
+# get file names
+filenames <- list.files(path = data_path, all.files = TRUE, pattern = "*.tsv",
+                        full.names = FALSE, recursive = FALSE,
+                        ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
+signature_filenames <- filenames[grepl(paste(signature_info$series_id, collapse = "|"), filenames)]
+
+##### get up/dn DE results #####
+library(tidyverse)
+library(here)
+
 technologies <- c("microarray", "RNAseq")
-directions <- c("up", "dn")
 
-##### FUNCTION: Split DE Results #####
-split_DE_results <- function(technology) {
+for (technology in technologies) {
 
+  # Load signature metadata
   metadata_path <- here(paste0("data/v2/signatures/", technology, "_TB_signature_run_info.tsv"))
-  signature_info <- read_tsv(metadata_path) %>% filter(signature == 1)
+  signature_info <- read_tsv(metadata_path) %>%
+    filter(signature == 1)
 
+  # Paths
   data_path <- here(paste0("data/v2/DE_results/", technology))
-  dir.create(file.path(data_path, "up"), showWarnings = FALSE, recursive = TRUE)
-  dir.create(file.path(data_path, "dn"), showWarnings = FALSE, recursive = TRUE)
+  up_folder <- file.path(data_path, "up")
+  dn_folder <- file.path(data_path, "dn")
 
-  filenames <- list.files(data_path, pattern = "\\.tsv$", full.names = FALSE)
+  # Create up/ and dn/ folders if missing
+  dir.create(up_folder, showWarnings = FALSE, recursive = TRUE)
+  dir.create(dn_folder, showWarnings = FALSE, recursive = TRUE)
+
+  # Get filenames matching series_ids
+  filenames <- list.files(path = data_path, pattern = "\\.tsv$", full.names = FALSE)
   signature_filenames <- filenames[grepl(paste(signature_info$series_id, collapse = "|"), filenames)]
 
+  # Process each signature file
   for (file in signature_filenames) {
-    de_df <- read_tsv(file.path(data_path, file))
 
+    # Read DE table
+    file_path <- file.path(data_path, file)
+    de_df <- read_tsv(file_path)
+
+    # Filter upregulated and downregulated
     up_genes <- de_df %>% filter(adj.P.Val < 0.05, log2FoldChange > 0)
     dn_genes <- de_df %>% filter(adj.P.Val < 0.05, log2FoldChange < 0)
 
+    # Create new filenames with suffixes
     base_filename <- sub("\\.tsv$", "", file)
-    write_tsv(up_genes, file.path(data_path, "up", paste0(base_filename, "-up.tsv")))
-    write_tsv(dn_genes, file.path(data_path, "dn", paste0(base_filename, "_dn.tsv")))
+    up_file <- file.path(up_folder, paste0(base_filename, "_up.tsv"))
+    dn_file <- file.path(dn_folder, paste0(base_filename, "_dn.tsv"))
+
+    # Save
+    write_tsv(up_genes, up_file)
+    write_tsv(dn_genes, dn_file)
   }
 }
 
+##########
 
-##### FUNCTION: Pathway Enrichment #####
-run_pathway_analysis <- function(technology, direction) {
+##### Pathway enrichment analysis #####
 
-  metadata_path <- here(paste0("data/v2/signatures/", technology, "_TB_signature_run_info.tsv"))
-  data_path <- here(paste0("data/v2/DE_results/", technology))
-  output_dir <- here(paste0("data/v2/", technology, "_pathways/", direction))
-  dir.create(output_dir, showWarnings = FALSE, recursive = TRUE)
+library(tidyverse)
+library(clusterProfiler)
+library(org.Hs.eg.db)
+library(here)
 
-  input_dir <- file.path(data_path, direction)
-  filenames <- list.files(input_dir, pattern = paste0("\\.", direction, "\\.tsv$"))
+directions <- c("up", "dn")
+metadata_path <- here(paste0("data/v2/signatures/", technology, "_TB_signature_run_info.tsv"))
+data_path <- here(paste0("data/v2/DE_results/", technology))
 
-  # Background genes
-  extension <- paste0("_", direction, ".tsv")
-  bg_genes_raw <- get_bg_genes(bg_source = "input data",
-                               metadata_path = metadata_path,
-                               data_path = data_path,
-                               extension = extension)
+for (direction in directions) {
 
-  bg_genes <- intersect(bg_genes_raw, GO_genes())
+  # Set output directory
+  dirname <- here(paste0("data/v2/", technology, "_pathways/", direction))
+  if (!dir.exists(dirname)) {
+    dir.create(dirname, recursive = TRUE)
+  }
 
-  # Iterate over DE result files
+  # Read gene files from up/dn folder
+  dirname_read <- file.path(data_path, direction)
+  filenames <- list.files(path = dirname_read, pattern = paste0(direction, ".tsv"))
+
+  # Define background genes
+  if(technology == "RNAseq"){extension <- paste0("_DESeq2_",direction,".tsv")}
+  if(technology == "microarray"){extension <- paste0("_limma_",direction,".tsv")}
+
+  extra_arg = ""
+  if (extra_arg == "") { extra_arg <- NULL }
+
+  bg_genes_source <- "input data"
+  bg_genes_ <- get_bg_genes(bg_source = bg_genes_source,
+                            metadata_path = metadata_path,
+                            data_path = dirname_read,
+                            extension = extension)
+
+  bg_genes <- intersect(bg_genes_, GO_genes())  # Restrict to GO-annotated genes
+
+  # Iterate through each file
+  itr <- 1
   for (file in filenames) {
-    genes_df <- read_tsv(file.path(input_dir, file))
 
-    gene_list <- as.character(genes_df$GeneID)
+    print(paste0("Running iteration: ", itr))
+    itr <- itr + 1
+
+    genes_df <- read_tsv(file.path(dirname_read, file))
+
+    # Use gene IDs as input for ORA
+    gene_vector <- genes_df$log2FoldChange
+    names(gene_vector) <- as.character(genes_df$GeneID)
+
+    gene_list <- names(gene_vector)
+
+    # Run ORA
+    print(paste0("Getting enriched GO pathways for ", file))
 
     enrichGO_res <- enrichGO(gene = gene_list,
                              OrgDb = org.Hs.eg.db,
@@ -75,25 +142,14 @@ run_pathway_analysis <- function(technology, direction) {
                              pvalueCutoff = 0.05,
                              qvalueCutoff = 0.1,
                              minGSSize = 5,
-                             maxGSSize = 200) %>%
-      as.data.frame()
+                             maxGSSize = 200)
 
+    enrichGO_res <- as.data.frame(enrichGO_res)
+
+    # Save results
     base_filename <- sub(paste0("\\.", direction, "\\.tsv$"), "", file)
-    output_file <- file.path(output_dir, paste0("GO_ORA_BGcorrected_", base_filename))
+    output_file <- file.path(dirname, paste0("GO_ORA_BGcorrected_", base_filename))
 
     write_tsv(enrichGO_res, file = output_file)
   }
 }
-
-
-##### PIPELINE #####
-for (technology in technologies) {
-  message("Splitting DE results for: ", technology)
-  split_DE_results(technology)
-
-  for (direction in directions) {
-    message("Running pathway analysis for: ", technology, " (", direction, ")")
-    run_pathway_analysis(technology, direction)
-  }
-}
-
