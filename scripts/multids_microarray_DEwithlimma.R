5d0f4d4b2a54a24a6ff0fecb44fec4011c1e2acb
Ling._.T <Lingthang03@gmail.com>
2025-07-15 13:30:10 -0600

    debug

--- scripts/multids_microarray_DEwithlimma.R
+++ scripts/multids_microarray_DEwithlimma.R
@@ -20,9 +20,9 @@ args[1] <- "data/v2/microarray_data_forDE/clean_TB_sample_metadata_classificatio
 args[2] <- 0.05
 
 # read in argument file
-meta_class_file_path = args[1]
-padj_cutoff = args[2]
-lincs_genes = read.delim(here("data/metadata/LINCSGeneSpaceSub.txt"), sep='\t')
+meta_class_file_path <- args[1]
+padj_cutoff <- args[2]
+lincs_genes <- read.delim(here("data/metadata/LINCSGeneSpaceSub.txt"), sep = "\t")
 
 signature_boolean <- list()
 platform_list <- list()
@@ -36,12 +36,12 @@ signature_boolean <- logical(nrow(study_df))
 up_genes_num <- integer(nrow(study_df))
 dn_genes_num <- integer(nrow(study_df))
 
-for(i in 1:nrow(study_df)){
+for (i in 1:nrow(study_df)) {
   # define tag i.e., study id
   tag <- study_df$series_id[i]
   # read in expression matrix
-  exprmat_path = paste0(study_df$EXPRMAT_PATH[i])
-  expr_mat <- read.delim(here(exprmat_path),sep = "\t")
+  exprmat_path <- paste0(study_df$EXPRMAT_PATH[i])
+  expr_mat <- read.delim(here(exprmat_path), sep = "\t")
 
   # standardise GSM IDs: trim whitespace and set to upper‑case
   colnames(expr_mat) <- trimws(toupper(colnames(expr_mat)))
@@ -56,8 +56,8 @@ for(i in 1:nrow(study_df)){
 
   # ---- 3.2  Attach classification and ensure matched GSMs ----
   # Keep only GSMs present in *both* metadata and matrix
-  common_ids_infected <- intersect(class_df[trimws(tolower(class_df$CLASSIFICATION)) == "disease without treatment",]$geo_accession, colnames(expr_mat))
-  common_ids_control <- intersect(class_df[trimws(tolower(class_df$CLASSIFICATION)) == "healthy control without treatment",]$geo_accession, colnames(expr_mat))
+  common_ids_infected <- intersect(class_df[trimws(tolower(class_df$CLASSIFICATION)) == "disease without treatment", ]$geo_accession, colnames(expr_mat))
+  common_ids_control <- intersect(class_df[trimws(tolower(class_df$CLASSIFICATION)) == "healthy control without treatment", ]$geo_accession, colnames(expr_mat))
 
   if ((length(common_ids_infected) < 3) | (length(common_ids_control) < 3)) {
     warning("  skipped: either infected or control condition has < 3 classified samples present in the count matrix")
@@ -66,18 +66,18 @@ for(i in 1:nrow(study_df)){
   }
 
   # get all the common ids
-  common_ids = c(common_ids_infected, common_ids_control)
+  common_ids <- c(common_ids_infected, common_ids_control)
   # Trim & reorder metadata and matrix to identical GSM sets
-  class_df <- metadata[match(common_ids, class_df$geo_accession), ]
+  class_df <- class_df[match(common_ids, class_df$geo_accession), ]
   expr_mat <- expr_mat[, common_ids, drop = FALSE]
 
   # ---- 3.3  Build the condition factor (now classification exists!) ----
   # get infected/control sample indices
-  inf_indices = which(trimws(tolower(class_df$CLASSIFICATION)) == "disease without treatment")
-  control_indices = which(trimws(tolower(class_df$CLASSIFICATION)) == "healthy control without treatment")
+  inf_indices <- which(trimws(tolower(class_df$CLASSIFICATION)) == "disease without treatment")
+  control_indices <- which(trimws(tolower(class_df$CLASSIFICATION)) == "healthy control without treatment")
 
   # Create classification labels
-  conditions = c()
+  conditions <- c()
   conditions[inf_indices] <- "infected"
   conditions[control_indices] <- "control"
 
@@ -85,7 +85,7 @@ for(i in 1:nrow(study_df)){
   class_df$CLASSIFICATION <- factor(conditions, levels = c("infected", "control"))
 
   # Platform batch info
-  platform_ids = unique(class_df$platform_id)
+  platform_ids <- unique(class_df$platform_id)
 
   if (length(platform_ids) > 1) {
     print("removing batch effect caused by platform")
@@ -95,9 +95,9 @@ for(i in 1:nrow(study_df)){
     class_df$platform_id <- factor(class_df$platform_id)
 
     # Use CLASSIFICATION and platform_id to build design matrix
-    design_mat = model.matrix(~0 + CLASSIFICATION + platform_id, data = class_df)
+    design_mat <- model.matrix(~ 0 + CLASSIFICATION + platform_id, data = class_df)
   } else {
-    design_mat = model.matrix(~0 + CLASSIFICATION, data = class_df)
+    design_mat <- model.matrix(~ 0 + CLASSIFICATION, data = class_df)
     print("no batch effect caused by GSE or platform")
   }
 
@@ -110,33 +110,33 @@ for(i in 1:nrow(study_df)){
   # apply linear model to the expression matrix
   fit <- lmFit(expr_mat, design = design_mat)
   # compute batch-corrected mean log expression
-  mean_exp = as.data.frame(fit$coefficients)
+  mean_exp <- as.data.frame(fit$coefficients)
 
   # apply empirical Bayes to smooth standard errors
   fit <- eBayes(fit)
 
   # correct biases by performing multiple testing, and obtain result table
-  stats_df <- topTable(fit, number = nrow(expr_mat),) %>% tibble::rownames_to_column("Gene")
+  stats_df <- topTable(fit, number = nrow(expr_mat), ) %>% tibble::rownames_to_column("Gene")
 
   ## add gene annotations
   # get gene annotation table
-  annot_path = paste0(here("data/metadata/Homo_sapiens.gene_info.csv"))
-  annotdata <- read.delim(annot_path,sep = ",")  # GeneID, Symbol, Ensembl
+  annot_path <- paste0(here("data/metadata/Homo_sapiens.gene_info.csv"))
+  annotdata <- read.delim(annot_path, sep = ",") # GeneID, Symbol, Ensembl
   entrezids <- stats_df$Gene
   annotdata_subset <- annotdata %>% filter(as.character(annotdata$GeneID) %in% entrezids)
-  annotdata_subset = annotdata_subset[,c('GeneID','Symbol','Ensembl')]
-  res_df =  merge(stats_df, annotdata_subset,  by.x = 'Gene', by.y = 'GeneID', all.x = TRUE, all.y = FALSE)
-  res_df = res_df[c("Gene","Symbol","infected","control","AveExpr","F","P.Value","adj.P.Val")]
+  annotdata_subset <- annotdata_subset[, c("GeneID", "Symbol", "Ensembl")]
+  res_df <- merge(stats_df, annotdata_subset, by.x = "Gene", by.y = "GeneID", all.x = TRUE, all.y = FALSE)
+  res_df <- res_df[c("Gene", "Symbol", "infected", "control", "AveExpr", "F", "P.Value", "adj.P.Val")]
   colnames(res_df)[1] <- "GeneID"
-  res_df$log2FoldChange = log2(res_df$infected) - log2(res_df$control)
+  res_df$log2FoldChange <- log2(res_df$infected) - log2(res_df$control)
 
   # identify duplicated genes (EntrezID)
   duplicates_booleans <- duplicated(res_df$GeneID)
   duplicated_genes <- res_df$GeneID[duplicates_booleans]
   duplicated_genes <- duplicated_genes[!is.na(duplicated_genes)]
 
-  if(length(duplicated_genes) != 0){
-    for(gene in duplicated_genes){
+  if (length(duplicated_genes) != 0) {
+    for (gene in duplicated_genes) {
       # look for its maximum logFC value and keep the max
       gene_indices <- which(res_df$GeneID == gene)
       logFC_vec <- res_df$log2FoldChange[gene_indices]
@@ -151,7 +151,7 @@ for(i in 1:nrow(study_df)){
   res_df <- res_df[!is.na(res_df$log2FoldChange), ]
 
   # ---- 3.6  Landmark genes filter ----
-  landmark_genes = as.character(lincs_genes[lincs_genes$Type == "landmark",]$Entrez.ID)
+  landmark_genes <- as.character(lincs_genes[lincs_genes$Type == "landmark", ]$Entrez.ID)
   sig_df <- dplyr::filter(res_df, GeneID %in% landmark_genes)
 
   # ---- 3.7  Significance filter ----
@@ -167,7 +167,7 @@ for(i in 1:nrow(study_df)){
   up_df <- dplyr::filter(sig_df, log2FoldChange > 0)
   dn_df <- dplyr::filter(sig_df, log2FoldChange < 0)
 
-  if(nrow(up_df) > 0){
+  if (nrow(up_df) > 0) {
     up_genes_num[i] <- nrow(up_df)
   }
   if (nrow(up_df) == 0) {
@@ -176,7 +176,7 @@ for(i in 1:nrow(study_df)){
     next
   }
 
-  if(nrow(dn_df) > 0){
+  if (nrow(dn_df) > 0) {
     dn_genes_num[i] <- nrow(dn_df)
   }
   if (nrow(dn_df) == 0) {
@@ -195,7 +195,7 @@ for(i in 1:nrow(study_df)){
       file.path(here::here("data/v2/DE_results/microarray"), paste0(base_fname, "_limma.tsv"))
     )
   }
-  if (nrow(sig_df) > 0){
+  if (nrow(sig_df) > 0) {
     readr::write_tsv(
       sig_df %>% arrange(desc(log2FoldChange)),
       file.path(here("data/v2/signatures/microarray/full"), paste0(base_fname, "_full.tsv"))
@@ -235,7 +235,3 @@ run_info <- file.path(
 )
 readr::write_tsv(study_df, run_info)
 message("\nFinished.  Summary written to ", run_info)
-
-
-
-
